ca7993f4a6c1631eab8a587aa4a41caf11cc0f22
Fielding Hoffmeier <Fieldinghoffmeier@gmail.com>
2026-04-22 14:26:48 -0600

    Grid

--- Assets/Grid Spawner Script.cs
+++ Assets/Grid Spawner Script.cs
@@ -5,6 +5,9 @@ public class GridSpawnerScript : MonoBehaviour
 {
     public GameObject gridObject;
     public int gridSize;
+    public Vector3 position;
+    public int gridScale;
+    public int maxGridObjectHeight;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,7 +16,7 @@ public class GridSpawnerScript : MonoBehaviour
         {
             for (int z = 0; z < gridSize; z++)
             {
-                Instantiate(gridObject, new Vector3(x * 2, 0, z * 2), quaternion.identity);
+                Instantiate(gridObject, new Vector3(x * gridScale, UnityEngine.Random.Range(1, maxGridObjectHeight) * 200/215 * 3, z * gridScale) + position, quaternion.identity);
             }
         }
     }
