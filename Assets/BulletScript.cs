e473a8f4bab057b7611d1b850f7e39b500b36691
Fielding Hoffmeier <Fieldinghoffmeier@gmail.com>
2026-05-25 13:52:01 -0600

    Made the enemy shoot bullets

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,38 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    public float bulletSpeed = 10;
+    public GameObject target;
+    public GameObject gun;
+    private Renderer renderer;
+    public float lifeTime;
+    private float lifeTimer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        transform.LookAt(target.transform.position + new Vector3(0, 1, 0));
+        lifeTimer = lifeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTimer -= Time.deltaTime;
+        transform.position += transform.forward * bulletSpeed * Time.deltaTime;
+        if (lifeTimer <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.gameObject != gun)
+        {
+           Destroy(gameObject); 
+        }
+    }
+}
--- Assets/DroneScript.cs
+++ Assets/DroneScript.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class DroneScript : MonoBehaviour
@@ -8,18 +10,46 @@ public class DroneScript : MonoBehaviour
     private Rigidbody rb;
     public float preferedDistance;
     public float stopZone;
+    public float fireRate;
+    private float shootTimer;
+    public GameObject bullet;
+    private int gunSequence = 1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         targetTransform = target.GetComponent<Transform>();
         rb = GetComponent<Rigidbody>();
+        shootTimer = 1/fireRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-         transform.LookAt(targetTransform);
+        transform.LookAt(targetTransform);
+        shootTimer = math.max(0, shootTimer - Time.deltaTime);
+        if (shootTimer <= 0)
+        {
+            shootTimer = 1/fireRate;
+
+            GameObject newBullet = Instantiate(bullet, 
+            transform.TransformPoint(new Vector3(5.5f * gunSequence, 0.5f, 7.5f)), 
+            quaternion.identity);
+
+            if (gunSequence == 1)
+            {
+                gunSequence = -1;
+            }
+            else
+            {
+                gunSequence = 1;
+            }
+
+            BulletScript BulletScript = newBullet.GetComponent<BulletScript>();
+            BulletScript.target = target;
+            BulletScript.gun = gameObject;
+        }
     }
+
     void FixedUpdate()
     {
         // Rotate through physics so it stays in sync with velocity
