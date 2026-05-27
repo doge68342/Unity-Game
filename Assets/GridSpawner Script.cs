cd8ff8ab38e6b5f0c967a94c803f3442ee1cee9a
Fielding Hoffmeier <Fieldinghoffmeier@gmail.com>
2026-05-27 14:02:06 -0600

    Made pillars move smoothly and fixed console error spamming

--- Assets/GridSpawner Script.cs
+++ Assets/GridSpawner Script.cs
@@ -14,33 +14,53 @@ public class GridSpawnerScript : MonoBehaviour
     public float randomizeInterval;
     public float randomizeTimer;
     private List<GameObject> pillars = new List<GameObject>();
+    private int[] targetPillarHeights;
+    private int[] currentPillarHeights;
+    public AnimationCurve movementCurve;
+    public float animationLength;
+    public float animationTimer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        randomizeTimer =  randomizeInterval;
+        randomizeTimer = 0;
         for (int x = 0; x < gridSize; x++)
         {
             for (int z = 0; z < gridSize; z++)
             {
-                GameObject clonedGridObject = Instantiate(gridObject, new Vector3(x * gridScale, Mathf.FloorToInt(MathF.Pow(UnityEngine.Random.value, 3f) * maxGridObjectHeight) * gridScale, z * gridScale) + position, quaternion.identity);
+                GameObject clonedGridObject = Instantiate(gridObject, new Vector3(x * gridScale, 0, z * gridScale) + position, quaternion.identity);
                 Renderer clonedRenderer = clonedGridObject.GetComponent<Renderer>();
                 clonedRenderer.material.color = Color.HSVToRGB(0, 0, UnityEngine.Random.Range(800f, 1000f) / 1000f);
                 pillars.Add(clonedGridObject);
             }
         }
+        targetPillarHeights = new int[pillars.Count];
+        currentPillarHeights = new int[pillars.Count];
     }
 
     // Update is called once per frame
     void Update()
     {
-        randomizeTimer -= Time.deltaTime;
+
+    }
+
+    void FixedUpdate()
+    {
+        randomizeTimer -= Time.fixedDeltaTime;
+        animationTimer -= Time.fixedDeltaTime;
         if (randomizeTimer <= 0)
         {
             randomizeTimer = randomizeInterval;
-            foreach (GameObject pillar in pillars)
+            animationTimer = animationLength;
+            for (int  i = 0; i < pillars.Count; i++)
             {
-                pillar.transform.position = new Vector3(pillar.transform.position.x, Mathf.FloorToInt(MathF.Pow(UnityEngine.Random.value, 3f) * maxGridObjectHeight) * gridScale + position.y, pillar.transform.position.z);
+                currentPillarHeights[i] = Mathf.FloorToInt(pillars[i].transform.position.y);
+                targetPillarHeights[i] = Mathf.FloorToInt(MathF.Pow(UnityEngine.Random.value, 3f) * maxGridObjectHeight);
             }
+        }
+        for (int i = 0; i < pillars.Count; i++)
+        {
+            // pillars[i].GetComponent<Rigidbody>().MovePosition(Vector3.Lerp(pillars[i].transform.position, new Vector3(pillars[i].transform.position.x, targetPillarHeights[i] * gridScale + position.y, pillars[i].transform.position.z), 1f - Mathf.Exp(-0.5f * Time.fixedDeltaTime)));
+            pillars[i].GetComponent<Rigidbody>().MovePosition(new Vector3(pillars[i].transform.position.x, Mathf.Lerp(currentPillarHeights[i], targetPillarHeights[i] * gridScale + position.y, movementCurve.Evaluate(1 - (animationTimer / animationLength))), pillars[i].transform.position.z));
         }
     }
 }
