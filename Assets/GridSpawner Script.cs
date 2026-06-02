55d8b4c41aa8aec171b1bfb0be0b0d3594e41e3c
Fielding Hoffmeier <Fieldinghoffmeier@gmail.com>
2026-06-02 11:22:41 -0600

    bouncepads

--- Assets/GridSpawner Script.cs
+++ Assets/GridSpawner Script.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GridSpawnerScript : MonoBehaviour
@@ -14,11 +15,16 @@ public class GridSpawnerScript : MonoBehaviour
     public float randomizeInterval;
     public float randomizeTimer;
     private List<GameObject> pillars = new List<GameObject>();
+    private List<GameObject> bouncePads = new List<GameObject>();
+    public int bouncePadsCount;
+    public int shortCount;
     private int[] targetPillarHeights;
     private int[] currentPillarHeights;
     public AnimationCurve movementCurve;
     public float animationLength;
     public float animationTimer;
+    public int bouncePadChance;
+    public GameObject bouncePad;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -40,7 +46,7 @@ public class GridSpawnerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        bouncePadsCount = bouncePads.Count;
     }
 
     void FixedUpdate()
@@ -51,15 +57,41 @@ public class GridSpawnerScript : MonoBehaviour
         {
             randomizeTimer = randomizeInterval;
             animationTimer = animationLength;
-            for (int  i = 0; i < pillars.Count; i++)
+            for (int i = 0; i < pillars.Count; i++)
             {
                 currentPillarHeights[i] = Mathf.FloorToInt(pillars[i].transform.position.y);
                 targetPillarHeights[i] = Mathf.FloorToInt(MathF.Pow(UnityEngine.Random.value, 3f) * maxGridObjectHeight);
             }
         }
+
+        if (animationTimer <= 0 && bouncePads.Count == 0)
+        {
+            Debug.Log(UnityEngine.Random.value);
+            shortCount = 0;
+            for (int i = 0; i < pillars.Count; i++)
+            {
+
+                if (Mathf.Approximately(pillars[i].transform.position.y, position.y)) shortCount++;
+                if (Mathf.Approximately(pillars[i].transform.position.y, position.y) && UnityEngine.Random.value * bouncePadChance <= 1f)
+                {
+                    GameObject clonedBouncePad = Instantiate(bouncePad);
+                    bouncePads.Add(clonedBouncePad);
+                    clonedBouncePad.transform.position = pillars[i].transform.position + new Vector3(0, 0.15f, 0);
+                }
+            }
+        }
+
+        if (animationTimer > 0)
+        {
+            for (int i = 0; i < bouncePads.Count; i++)
+            {
+                Destroy(bouncePads[i]);
+            }
+            bouncePads.Clear();
+        }
+
         for (int i = 0; i < pillars.Count; i++)
         {
-            // pillars[i].GetComponent<Rigidbody>().MovePosition(Vector3.Lerp(pillars[i].transform.position, new Vector3(pillars[i].transform.position.x, targetPillarHeights[i] * gridScale + position.y, pillars[i].transform.position.z), 1f - Mathf.Exp(-0.5f * Time.fixedDeltaTime)));
             pillars[i].GetComponent<Rigidbody>().MovePosition(new Vector3(pillars[i].transform.position.x, Mathf.Lerp(currentPillarHeights[i], targetPillarHeights[i] * gridScale + position.y, movementCurve.Evaluate(1 - (animationTimer / animationLength))), pillars[i].transform.position.z));
         }
     }
